9007a83d50a23a505432cc153e843f3a451ac4cd
VickyhqL <89110625+VickyhqL@users.noreply.github.com>
2021-10-25 09:08:52 +0800

    Update quiz_003.py

--- quiz_003.py
+++ quiz_003.py
@@ -1,4 +1,20 @@
 N=
-for i in range(100):
-  result+=str(i%N)+","
+def rp(N):
+  result=""
+  for i in range(100):
+    result+=str(i%N)+","
+ return rp(N)
+
+
+age=
+def age(age):
+  result=""
+  counter=1
+  for i in range (100):
+    result+=str(counter)+","
+    if counter >=N:
+      counter=1
+    else:
+      counter+1
+
 
